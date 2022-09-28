b0f615dae1527db042617a214a906c82febb1fcb
DoniSetiawan01 <sdoni3801@gmail.com>
2022-09-28 01:50:49 -0700

    add repositori

---
+++ pembanding.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projekpertama;
+
+/**
+ *
+ * @author user
+ */
+public class pembanding {
+    public static void main(String[] args){
+        String kata1, kata2;
+        kata1 = "Doni";
+        kata2 = "Doni";
+        System.out.println(kata1.compareTo(kata2));
+        
+    }
+    
+}
